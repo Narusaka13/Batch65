51d1f62cf4247c589cbde4dd1007894083d365ad
Narusaka13 <ridhonaufalpribadi@gmail.com>
2026-02-02 10:57:08 +0700

    modify index.js, html project page, and style.css

--- index.js
+++ index.js
@@ -10,3 +10,53 @@ function navigateTo(page) {
   // Go to the page
   window.location.href = pages[page] || "HomePage.html";
 }
+// input.js
+// input receiver
+const addproject = document.getElementById("addproject");
+const userlist = document.getElementById("userlist");
+
+// temp memory
+let projects = [];
+// event submit form
+addproject.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let projectname = document.getElementById("ProjectName").value;
+  let start = document.getElementById("start-date").value;
+  let end = document.getElementById("end-date").value;
+  let desc = document.getElementById("description").value;
+
+  const project = { projectname, start, end, desc };
+  projects.push(project);
+  console.log(projects);
+
+  changeElement();
+  renderUsers();
+});
+
+// Header change
+function changeElement(parameters) {
+  document.getElementById(
+    "input-user"
+  ).innerHTML = `<p>New project has been added!!</p>`;
+}
+
+function renderUsers(parameters) {
+  userlist.innerHTML = "";
+
+  for (let i = 0; i < projects.length; i++) {
+    userlist.innerHTML += `
+    <div class="Project p-2 mb-3">
+    <h5>Project : ${projects[i].projectname}</h5>
+    <h5>
+      Duration : <span><h5>${projects[i].start} => ${projects[i].end}</h5></span>
+    </h5>
+    <h5>
+      Project description:
+      <span>
+        <p>${projects[i].desc}</p>
+      </span>
+    </h5>
+  </div>`;
+  }
+}
