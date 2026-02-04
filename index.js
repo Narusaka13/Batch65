d0f0478fef6a8770008570ae0a083bcbeb13821a
Narusaka13 <ridhonaufalpribadi@gmail.com>
2026-02-04 15:27:41 +0700

    Feat : Add alert at home, send element to hbs

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // const express = require("express");
 import express from "express";
 const app = express();
-const port = 3000;
+const port = "3000";
 
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 app.use("/assets", express.static("src/assets"));
-app.get("/", (req, res) => {
-  //   res.send("Hello World!!!Narusaka here!!!");
-  res.render("homepage");
-});
+// app.get("/", (req, res) => {
+//   //   res.send("Hello World!!!Narusaka here!!!");
+//   res.render("homepage");
+// });
+app.get("/", home);
 app.get("/contact", (req, res) => {
-  res.render("Contactpage");
+  const phoneNumber = 6287839056763;
+  res.render("Contactpage", { phoneNumber });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+// function
+function home(req, res) {
+  res.render("Homepage");
+}
