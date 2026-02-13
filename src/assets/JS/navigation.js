1d0fff6edac7993cf9b0a42ec9947acb47a2db81
Narusaka13 <ridhonaufalpribadi@gmail.com>
2026-02-13 22:00:48 +0700

    feat:rendering card projects, seperated nav logic

---
+++ src/assets/JS/navigation.js
@@ -0,0 +1,17 @@
+// LOGIC FOR navigation.js //
+function navigateTo(page, projectId = null) {
+  // Map pages URLs
+  const pages = {
+    home: "/",
+    projects: "/projects",
+    contact: "/contact",
+    detail: "/detail",
+  };
+  // Go to the page with navigating to detail page
+  if (page === "detail" && projectId) {
+    window.location.href = `${pages.detail}?id=${projectId}`;
+  } else {
+    window.location.href = pages[page] || "/";
+  }
+}
+export { navigateTo };
